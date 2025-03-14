651c8810a1d51708e7a654488be3f1b5e4d7cb2c
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-03-14 13:39:08 +0500

    .

--- pro-dasturlash-tests/31dars.py
+++ pro-dasturlash-tests/31dars.py
@@ -1,14 +1,51 @@
-import sqlite3 as sq
+import sqlite3
+from abc import ABC, abstractmethod
+from contextlib import closing
 
-conn = sq.connect("sample-database.db")
-cur = conn.cursor()
 
-cur.execute("""
-SELECT * FROM employees LIMIT 5;
-""")
-ans = cur.fetchall()
-for i in ans:
-    print(i)
+class BaseCRUD(ABC):
+    def __init__(self, database_path, table_name):
+        self.database_path = database_path
+        self.table_name = table_name
 
-cur.close()
-conn.close()
+    def get_connection(self):
+        return closing(sqlite3.connect(self.database_path))
+
+    def insert(self, **kwargs):
+        with self.get_connection() as connection:
+            cursor = connection.cursor()
+            columns = ', '.join(kwargs.keys())
+            placeholders = ', '.join('?' for _ in kwargs)
+            query = f"INSERT INTO {self.table_name} ({columns}) VALUES ({placeholders})"
+            cursor.execute(query, tuple(kwargs.values()))
+            connection.commit()
+            return cursor.lastrowid
+
+    def get(self, id, id_column="id"):
+        with self.get_connection() as connection:
+            cursor = connection.cursor()
+            query = f"SELECT * FROM {self.table_name} WHERE {id_column}=?"
+            cursor.execute(query, (id,))
+            return cursor.fetchone()
+
+    def update(self, id, id_column="id", **kwargs):
+        with self.get_connection() as connection:
+            cursor = connection.cursor()
+            columns = ', '.join(f"{key}=?" for key in kwargs)
+            query = f"UPDATE {self.table_name} SET {columns} WHERE {id_column}=?"
+            cursor.execute(query, (*kwargs.values(), id))
+            connection.commit()
+
+    def delete(self, id, id_column="id"):
+        with self.get_connection() as connection:
+            cursor = connection.cursor()
+            query = f"DELETE FROM {self.table_name} WHERE {id_column}=?"
+            cursor.execute(query, (id,))
+            connection.commit()
+
+employee_crud = BaseCRUD("sample-database (5).db", "employees")
+employee_id = employee_crud.insert(first_name="John", last_name="Doe", email="qwasar@mail.ru")
+employee = employee_crud.get(id=101, id_column="employee_id")
+print(employee)
+# employee_crud.update(employee_id, bio="An updated bio")
+# employee_crud.delete(employee_id)
