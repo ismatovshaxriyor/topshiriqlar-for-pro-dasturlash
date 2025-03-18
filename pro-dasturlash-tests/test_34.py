bf6246f83a6772392fe769de3192c1d6524e8d9c
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-03-18 21:29:25 +0500

    .

--- pro-dasturlash-tests/test_34.py
+++ pro-dasturlash-tests/test_34.py
@@ -24,12 +24,17 @@ def start_cmd(update, context):
     update.message.reply_text(text=f"salom {update.message.from_user.first_name}")
     context.bot.send_message(chat_id=ADMIN_ID, text=f"Yangi user: {update.message.from_user.first_name}")
 
+def message_handler(update, context):
+    update.message.reply_text(text=f"sizni tushunmadim {update.message.from_user.first_name}")
+
 def main():
     TOKEN = "8062600526:AAEis5onPOo9CV15H-2abjXGH1GDC_fYF44"
     updater = Updater(token=TOKEN, request_kwargs={'connect_timeout': 30, 'read_timeout': 30})
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start_cmd))
     dispatcher.add_handler(CommandHandler("menu", show_menu))
+    dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
+    # dispatcher.add_handler(MessageHandler("menu", show_menu))
 
     updater.start_polling()
     updater.idle()
