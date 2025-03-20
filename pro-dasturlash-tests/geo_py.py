1e5c7c79913a531fcf7ea5e3078dce9c8e6e4399
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-03-20 21:22:18 +0500

    .

--- pro-dasturlash-tests/geo_py.py
+++ pro-dasturlash-tests/geo_py.py
@@ -1,1 +1,8 @@
-from 
+from geopy.geocoders import Nominatim
+
+def get_location(latitude, longitude):
+    geolocator = Nominatim(user_agent="my_app")
+    location = geolocator.reverse(f"{latitude}, {longitude}")
+    return location.address
+
+print(get_location(41.32188, 69.22083))
